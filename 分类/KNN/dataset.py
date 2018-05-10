626bdf8b3c8c9f101925f217ea223224f24fd2f6
lipeng29 <Abc!014916>
2018-05-10 17:11:14 +0800

    ~

--- 分类/KNN/dataset.py
+++ 分类/KNN/dataset.py
@@ -1,6 +1,9 @@
 # -* - coding: UTF-8 -* -
 
-import numpy
+from numpy import *
+import operator
+
+datasetfilepath='/home/lipeng/.keras/datasets/datingTestSet2.txt'
 
 def file2matrix(filename):
    """
@@ -16,7 +19,7 @@ def file2matrix(filename):
    numberOfLines = len(fr.readlines())
    # 生成对应的空矩阵
    # 例如：zeros(2，3)就是生成一个 2*3的矩阵，各个位置上全是 0
-   returnMat = numpy.zeros((numberOfLines, 3))  # prepare matrix to return
+   returnMat =   zeros((numberOfLines, 3))  # prepare matrix to return
    classLabelVector = []  # prepare labels return
    fr = open(filename)
    index = 0
@@ -32,3 +35,89 @@ def file2matrix(filename):
        index += 1
    # 返回数据矩阵returnMat和对应的类别classLabelVector
    return returnMat, classLabelVector
+
+def autoNorm(dataSet):
+    """
+    Desc:
+        归一化特征值，消除特征之间量级不同导致的影响
+    parameter:
+        dataSet: 数据集
+    return:
+        归一化后的数据集 normDataSet. ranges和minVals即最小值与范围，并没有用到
+
+    归一化公式：
+        Y = (X-Xmin)/(Xmax-Xmin)
+        其中的 min 和 max 分别是数据集中的最小特征值和最大特征值。该函数可以自动将数字特征值转化为0到1的区间。
+    """
+    # 计算每种属性的最大值、最小值、范围
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    # 极差
+    ranges = maxVals - minVals
+    normDataSet = zeros( shape(dataSet))
+    m = dataSet.shape[0]
+    # 生成与最小值之差组成的矩阵
+    normDataSet = dataSet -  tile(minVals, (m, 1))
+    # 将最小值之差除以范围组成矩阵
+    normDataSet = normDataSet / tile(ranges, (m, 1))  # element wise divide
+    return normDataSet, ranges, minVals
+
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    # 距离度量 度量公式为欧氏距离
+    diffMat = tile(inX,(dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat ** 2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances ** 0.5
+
+    # 将距离排序：从小到大
+    sortedDistIndicies = distances.argsort()
+    # 选取前K个最短距离， 选取这K个中最多的分类类别
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+def datingClassTest():
+    """
+    Desc:
+        对约会网站的测试方法
+    parameters:
+        none
+    return:
+        错误数
+    """
+    # 设置测试数据的的一个比例（训练数据集比例=1-hoRatio）
+    hoRatio = 0.1  # 测试范围,一部分测试一部分作为样本
+    # 从文件中加载数据
+    datingDataMat, datingLabels = file2matrix(datasetfilepath)  # load data setfrom file
+    # 归一化数据
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    # m 表示数据的行数，即矩阵的第一维
+    m = normMat.shape[0]
+    # 设置测试的样本数量， numTestVecs:m表示训练样本的数量
+    numTestVecs = int(m * hoRatio)
+    print 'numTestVecs=', numTestVecs
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        # 对数据测试
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
+        print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i])
+        if (classifierResult != datingLabels[i]): errorCount += 1.0
+    print "the total error rate is: %f" % (errorCount / float(numTestVecs))
+    print errorCount
+
+def classifyPerson():
+    resultList = ['not at all', 'in small doses', 'in large doses']
+    percentTats = float(raw_input("percentage of time spent playing video games ?"))
+    ffMiles = float(raw_input("frequent filer miles earned per year?"))
+    iceCream = float(raw_input("liters of ice cream consumed per year?"))
+    datingDataMat, datingLabels = file2matrix( datasetfilepath )
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    inArr = array([ffMiles, percentTats, iceCream])
+    classifierResult = classify0((inArr-minVals)/ranges,normMat,datingLabels, 3)
+    print "You will probably like this person: ", resultList[classifierResult - 1]
+classifyPerson()
